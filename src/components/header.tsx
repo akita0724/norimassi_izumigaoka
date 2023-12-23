da7826f5402b3be74c278ce2b9fd927a7e9da433
akita0724 <118238952+akita0724@users.noreply.github.com>
2023-12-23 17:03:52 +0900

    fixed header issue

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <nav className="header text-center select-none">
+    <nav className="header text-center select-none z-10">
       <p className="text-2xl text-slate-200">のりまっし泉丘</p>
     </nav>
   );
