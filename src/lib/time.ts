b621acff5bf802b30fb295ac3542c0a2cee8a7de
akita0724 <118238952+akita0724@users.noreply.github.com>
2023-12-23 15:34:11 +0900

    fixed tz issue

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -1,25 +1,36 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
 export const calUnix = (date: string, time: string) => {
 
     let [year, month, day] = date.split('-').map(val => Number(val || 0));
     let [hour, minute] = time.split(':').map(val => Number(val || 0));
 
-    const now = new Date();
 
+    const now = useTime()
     if (year == 0) {
-        year = now.getFullYear();
-        month = now.getMonth() + 1;
-        day = now.getDate();
+        year = now.year();
+        month = now.month() + 1;
+        day = now.date();
     }
     if (time == "") {
-        hour = now.getHours()
-        minute = now.getMinutes()
+        hour = now.hour()
+        minute = now.minute()
     }
 
-    const unix = Math.floor(new Date(year, month - 1, day, hour, minute).getTime() / 1000);
+    const unix = dayjs(`${year}-${("00" + month).slice(-2)}-${("00" + day).slice(-2)} ${("00" + hour).slice(-2)}:${("00" + minute).slice(-2)}:00`)
 
-    return unix;
+    return unix.unix();
 }
 
 export const displayTime = (time: number): string => {
     return `${Math.floor(time / 100)} : ${("00" + time % 100).slice(-2)}`
+}
+
+export const useTime = () => {
+    dayjs.extend(utc);
+    dayjs.extend(timezone);
+    const date = dayjs().tz("Asia/Tokyo");
+    return date
 }
